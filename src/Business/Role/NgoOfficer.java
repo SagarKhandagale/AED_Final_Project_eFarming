e11a761f30352fc27861053a90f73da61b1e8c19
prachikunjir11 <90424565+prachikunjir11@users.noreply.github.com>
2021-12-12 16:30:11 -0500

    added constructor

--- src/Business/Role/NgoOfficer.java
+++ src/Business/Role/NgoOfficer.java
@@ -22,7 +22,7 @@ public class NgoOfficer extends Role {
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,Network network, EcoSystem business) {
 
-        return new PanelNgoOfficerWorkArea();
+        return new PanelNgoOfficerWorkArea(userProcessContainer,account,organization,enterprise,network,business);
     }
 
     @Override
