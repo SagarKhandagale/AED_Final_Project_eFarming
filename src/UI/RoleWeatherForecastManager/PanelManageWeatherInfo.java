d2926b174c308f92f1eda9ae84c5ce206e2ec59b
prachikunjir11 <90424565+prachikunjir11@users.noreply.github.com>
2021-12-12 18:01:28 -0500

    transporter and waether pages

--- src/UI/RoleWeatherForecastManager/PanelManageWeatherInfo.java
+++ src/UI/RoleWeatherForecastManager/PanelManageWeatherInfo.java
@@ -5,17 +5,29 @@
  */
 package UI.RoleWeatherForecastManager;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Kunjir
  */
 public class PanelManageWeatherInfo extends javax.swing.JPanel {
-
+    private JPanel userProcessContainer;
+    private EcoSystem system;
+    private UserAccount userAccount;
+    private Enterprise enterprise;
     /**
      * Creates new form PanelManageWeatherInfo
      */
-    public PanelManageWeatherInfo() {
+    public PanelManageWeatherInfo(JPanel userProcess, EcoSystem system, Enterprise enterprise, UserAccount userAccount) {
         initComponents();
+        this.userProcessContainer = userProcess;
+        this.system = system;
+        this.enterprise = enterprise;
+        this.userAccount = userAccount;
     }
 
     /**
