d381c2c88979e588d5760d0257bf5453039fea27
Ritujauni <91397743+Ritujauni@users.noreply.github.com>
2021-12-12 13:35:37 -0500

    Farmer Order Logic

--- src/Business/Role/ShopOwner.java
+++ src/Business/Role/ShopOwner.java
@@ -22,7 +22,7 @@ public class ShopOwner extends Role {
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,Network network, EcoSystem business) {
 
-        return new PanelShopOwnerWorkArea();
+        return new PanelShopOwnerWorkArea(userProcessContainer,account,organization,enterprise,network,business);
     }
 
     @Override
